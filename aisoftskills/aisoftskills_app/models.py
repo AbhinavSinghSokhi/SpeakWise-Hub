b8b1de8b706642088c9b749595254f8cc925a8ea
Debasish Raut <debasishraut1996@outlook.com>
2024-04-01 17:58:45 +0530

    Removed ignored files

--- aisoftskills/aisoftskills_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-class paragraph(models.Model):
-    para_num= models.IntegerField(unique= True)
-    paragraph=models.TextField()
-
-    def __str__(self):
-        return f"Paragraph {self.para_num}"
